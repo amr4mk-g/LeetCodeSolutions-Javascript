20c28f5ba0a7a23fbf1cb82d06d2904ef50f1122
Amr Muhammad <47480508+amr4mk-g@users.noreply.github.com>
2023-02-23 12:08:56 +0200

    Create 2359. Find Closest Node to Given Two Nodes.js

---
+++ 2359. Find Closest Node to Given Two Nodes.js
@@ -0,0 +1,27 @@
+function closestMeetingNode (edges, node1, node2) {
+     let n = edges.length
+    let map1 = new Array(n).fill(-1)
+    let map2 = new Array(n). fill(-1)
+    map1[node1] = 0
+    map2[node2] = 0
+
+    while (edges[node1] != -1 && map1[edges[node1]] == -1) {
+        map1[edges[node1]] = map1[node1] + 1
+        node1 = edges[node1]
+    }
+    while (edges[node2] != -1 && map2[edges[node2]] == -1) {
+        map2[edges[node2]] = map2[node2] + 1
+        node2 = edges[node2]
+    } 
+
+    let res = n, node = -1
+    for (let i = 0; i < n; i++) {
+        if (map1[i] == -1 || map2[i] == -1) continue
+        let val = Math.max(map1[i], map2[i])
+        if (res > val) {
+            res = val
+            node = i
+        }
+    }
+    return node
+}
