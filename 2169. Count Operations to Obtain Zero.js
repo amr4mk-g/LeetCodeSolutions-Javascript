e03ece8733d98fe7b38e11d6e78e2e94c85895b6
Amr Muhammad <47480508+amr4mk-g@users.noreply.github.com>
2023-02-23 18:35:28 +0200

    Create 2169. Count Operations to Obtain Zero.js

---
+++ 2169. Count Operations to Obtain Zero.js
@@ -0,0 +1,10 @@
+function countOperations (num1, num2) {
+    if (num1== 0 || num2==0) return 0;
+    let opt = 0;
+    while (!(num1== 0 || num2==0)) {
+       if (num1 >= num2) num1 -= num2;
+       else num2 -= num1;
+       opt++;
+    }
+    return opt;
+}
