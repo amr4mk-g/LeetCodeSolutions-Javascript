2a0f56d5d91c37052f4a3d540977a5df7cd1f214
Amr Muhammad <47480508+amr4mk-g@users.noreply.github.com>
2023-02-23 19:00:39 +0200

    Create 0014. Longest Common Prefix.js

---
+++ 0014. Longest Common Prefix.js
@@ -0,0 +1,9 @@
+function longestCommonPrefix(strs) {
+  if (!strs.length) return '';
+  for (let i=0; i<strs[0].length; i++) {
+     for (let str of strs) {
+        if (str[i] != strs[0][i]) return str.slice(0, i);
+    }
+  }
+  return strs[0];
+}
