b9fdb2d892b55701ca41c1c1470f4edc411b79fb
Amr Muhammad <47480508+amr4mk-g@users.noreply.github.com>
2023-02-23 12:54:32 +0200

    Create 1346. Check If N and Its Double Exist.js

---
+++ 1346. Check If N and Its Double Exist.js
@@ -0,0 +1,8 @@
+function checkIfExist (arr) {
+     for(let i=0;i<arr.length;i++){
+        if(arr.indexOf(arr[i]/2) !== -1 && arr.indexOf(arr[i]/2) !== i){
+            return true;
+        }
+    }
+    return false;
+}
