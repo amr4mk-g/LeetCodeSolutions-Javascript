35e7c3f34f09e3c60efd5615b0c52cc0de03b19d
Amr Muhammad <47480508+amr4mk-g@users.noreply.github.com>
2023-02-27 10:38:09 +0200

    Create 0066. Plus One.js

---
+++ 0066. Plus One.js
@@ -0,0 +1,11 @@
+function plusOne (digits) {
+    let n = digits.length;
+    for (let i=n-1; i>=0; i--){
+        if (digits[i] == 9) digits[i] = 0;
+        else {
+          digits[i]++;
+          return digits;
+        }
+    }
+    return [1, ...digits];
+}
