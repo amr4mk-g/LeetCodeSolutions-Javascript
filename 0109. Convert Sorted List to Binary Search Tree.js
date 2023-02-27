eca162cdb50661f487932541135ece7364847499
Amr Muhammad <47480508+amr4mk-g@users.noreply.github.com>
2023-02-27 10:57:24 +0200

    Create 0109. Convert Sorted List to Binary Search Tree.js

---
+++ 0109. Convert Sorted List to Binary Search Tree.js
@@ -0,0 +1,18 @@
+function sortedListToBST (head) {
+    let arr = [];
+    while (head) {
+        arr.push(head.val);
+        head = head.next;
+    }
+    return sortedArrayToBST(arr);
+}
+
+function sortedArrayToBST (arr) {
+    function helper(l, r){
+        if(l > r) return null;
+        let mid = Math.floor((l+r)/2);
+        let node = new TreeNode(arr[mid], helper(l, mid-1), helper(mid+1, r));
+        return node;
+    }
+    return helper(0, arr.length-1);
+}
