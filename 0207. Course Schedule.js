e2fdaa77f1e4c76b29f17903bb1d8d0d1b696a8a
Amr Muhammad <amr4mk@gmail.com>
2023-05-16 22:30:30 +0300

    Create 0207. Course Schedule.js

---
+++ 0207. Course Schedule.js
@@ -0,0 +1,20 @@
+function canFinish(numCourses, prerequisites) {
+    let n = numCourses;
+    let inDegree = new Array(n).fill(0);
+    let queue = [];
+    let result = []; 
+    for (let v of prerequisites) inDegree[v[0]]++;
+    for (let i=0; i<n; i++) if (inDegree[i] == 0) queue.push(i);
+    while (queue.length) {
+        let curr = queue.pop();
+        n--;
+        result.push(curr);
+        for (let [course, pre] of prerequisites) {
+            if (curr == pre) {
+                inDegree[course]--;
+                if (inDegree[course] == 0) queue.push(course);
+            }
+        }
+    }
+    return result.length == numCourses;
+}
