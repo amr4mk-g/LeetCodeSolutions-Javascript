ea3ad073f3e479089fad4f84c3c9924071cea150
Amr Muhammad <amr4mk@gmail.com>
2023-05-16 22:02:13 +0300

    Create 162. Find Peak Element.js

---
+++ 162. Find Peak Element.js
@@ -0,0 +1,9 @@
+function findPeakElement (arr) {
+      let l = 0, r = arr.length-1;
+      while (l <= r) {
+        let m = Math.floor((l+r)/2);
+        if (arr[m] < arr[m+1]) l = m+1;
+        else r = m-1;
+      }
+      return l;
+}
