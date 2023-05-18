b65309d7df286ea3f10a164c487a5ec3f58636ff
Amr Muhammad <amr4mk@gmail.com>
2023-05-18 21:57:25 +0300

    Create 0344. Reverse String.js

---
+++ 0344. Reverse String.js
@@ -0,0 +1,9 @@
+function reverseString (s) {
+    let len = s.length;
+    let left = 0, right = len-1;
+    while (left < right) {
+        [s[left], s[right]] = [s[right], s[left]];
+        left++;  right--;
+    }
+    return s;
+}
